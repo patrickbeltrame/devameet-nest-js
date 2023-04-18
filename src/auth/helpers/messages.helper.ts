0f145106d6237c4d4fe31d1b8631412c7c52fd3c
Patrick Rodrigues <83885685+patrickbeltrame@users.noreply.github.com>
2023-04-18 11:11:19 -0300

    conclusão api de login

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+    AUTH_LOGIN_NOT_FOUND: 'Login informado nao e valido',
+    AUTH_PASSWORD_NOT_FOUND: 'Senha informada nao e valida',
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'Email e/ou senha nao encontrados',
+}
