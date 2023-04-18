cf08f96a304bd623d715a00b64140746b1d12e79
Patrick Rodrigues <83885685+patrickbeltrame@users.noreply.github.com>
2023-04-18 16:58:48 -0300

    Criação e implementação dos guards e decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.services";
 import { LoginDto } from "./dtos/login.dto";
 import { RegisterDto } from "src/user/dtos/register.dto";
+import { IsPublic } from "./decorators/ispublic.decorator";
 
 @Controller('auth')
 export class AuthController {
@@ -9,12 +10,14 @@ export class AuthController {
     
     @Post('login')
     @HttpCode(HttpStatus.OK)
+    @IsPublic()
     login(@Body() dto: LoginDto){
         return this.authService.login(dto);
     }
 
     @Post('register')
     @HttpCode(HttpStatus.OK)
+    @IsPublic()
     register(@Body() dto: RegisterDto){
         return this.authService.register(dto);
     }
--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -4,21 +4,33 @@ import { MessagesHelper } from "./helpers/messages.helper";
 import { RegisterDto } from "src/user/dtos/register.dto";
 import { UserService } from "src/user/user.service";
 import { UserMessagesHelper } from "src/user/helpers/messages.helper";
+import { JwtService } from "@nestjs/jwt";
 
 
 @Injectable()
 export class AuthService {
     private logger = new Logger(AuthService.name);
 
-    constructor(private readonly userService: UserService){}
+    constructor(private readonly userService: UserService,
+        private readonly jwtService: JwtService){}
 
-    login(dto: LoginDto){
-        this.logger.debug('login - started')
-        if(dto.login !== 'teste@teste.com' ||
-        dto.password !== 'teste@123'){
+    async login(dto: LoginDto){
+        this.logger.debug('login - started');
+
+        const user = await this.userService.getUserByLoginPassword(dto.login, dto.password);
+
+        if(user == null){
             throw new BadRequestException(
                 MessagesHelper.AUTH_PASSWORD_OR_LOGIN_NOT_FOUND
             )
+        }
+
+        const tokenPayLoad = {email: user.email, sub: user._id};
+
+        return {
+            email: user.email,
+            name: user.name,
+            token: this.jwtService.sign(tokenPayLoad, {secret: process.env.USER_JWT_SECRET_KEY})
         }
         return dto;
     }
